110870aa9cbfdef7fba4f97389baf36b4c9fda03
le codeur masqué <44831881+karlmenino@users.noreply.github.com>
2021-08-29 16:29:11 +0200

    trouver poathologies par nom/hopitaux par specialité

--- src/main/java/com/openclassroom/p11/dao/HopitalDao.java
+++ src/main/java/com/openclassroom/p11/dao/HopitalDao.java
@@ -1,6 +1,7 @@
 package com.openclassroom.p11.dao;
 
 import com.openclassroom.p11.model.Hopital;
+import com.openclassroom.p11.model.Specialite;
 import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,5 @@ import java.util.List;
 @Repository
 @DynamicUpdate
 public interface HopitalDao extends CrudRepository<Hopital,Long> {
-    public List<Hopital> findAllBySpecialitesIsContaining (String name);
+    public List<Hopital> findAllBySpecialitesIsContaining (Specialite specialite);
 }
--- src/main/java/com/openclassroom/p11/dao/PathologiesDao.java
+++ src/main/java/com/openclassroom/p11/dao/PathologiesDao.java
@@ -1,12 +1,9 @@
 package com.openclassroom.p11.dao;
 
 import com.openclassroom.p11.model.Pathologies;
-import com.openclassroom.p11.model.Specialite;
 import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.ArrayList;
 import java.util.Optional;
 
 @Repository
